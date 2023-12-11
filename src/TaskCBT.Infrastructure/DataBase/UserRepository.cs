ab991a3bb47fa01d515dde3302ca3fe7de339594
Lime <demn2001@gmail.com>
2023-12-11 04:48:04 +0300

    add auth data to user in create

--- src/TaskCBT.Infrastructure/DataBase/UserRepository.cs
+++ src/TaskCBT.Infrastructure/DataBase/UserRepository.cs
@@ -10,11 +10,12 @@ public class UserRepository(ICurrentUser current, IContext context) : IUserRepos
         if (current.AuthID is not int authId) return false;
         await using var transaction = await context.DbContext.Database.BeginTransactionAsync(cancellationToken);
         Auth? auth = await context.Auths.FindAsync([authId], cancellationToken);
-        if (auth?.User is null) return false;
+        if (auth is null || auth.User is not null) return false;
         User user = new User
         {
             FirstName = firstName,
-            LastName = lastName
+            LastName = lastName,
+            Auth = auth
         };
         await context.Users.AddAsync(user, cancellationToken);
         await context.DbContext.SaveChangesAsync(cancellationToken);
