77d26a579562e26e1f2970abe81ceb257e029869
Lime <demn2001@gmail.com>
2023-12-12 19:43:09 +0300

    done web.app

--- src/TaskCBT.WebApp/wwwroot/js/main.js
+++ src/TaskCBT.WebApp/wwwroot/js/main.js
@@ -1,3 +1,7 @@
+const claims = {
+    role: 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'
+}
+
 /**
  * @param {string} api
 */
@@ -7,6 +11,15 @@ async function preloadUser(api) {
     if (jwt === undefined) {
         window.location = 'auth';
         return undefined;
+    }
+    if (claims.role in jwt) {
+        let role = jwt[claims.role];
+        if (role === 'create') {
+            if (window.location.href !== 'create') {
+                window.location = 'create';
+            }
+            return undefined;
+        }
     }
     let response = await fetch(`${api}/users/0`, { jwt: 'local' });
     if (response.status !== 200) {
@@ -19,6 +32,21 @@ async function preloadUser(api) {
         return undefined;
     }
     return json.response;
+}
+/**
+ * @param {string} api
+*/
+function preloadCreate(api) {
+    initFetchJwt(`${api}/auth/refresh`, () => window.location = 'auth');
+    let jwt = getParsedCurrentJwt();
+    if (jwt === undefined) {
+        window.location = 'auth';
+        return false;
+    }
+    if (claims.role in jwt && jwt[claims.role] === 'create')
+        return true;
+    window.location = '/';
+    return false;
 }
 /**
  * @param {{tag:string, options?:ElementCreationOptions, child?:tree, childs?:Array<tree>, setup?:function(HTMLElement)}} tree
@@ -42,17 +70,24 @@ function deepClone(obj) {
     return JSON.parse(JSON.stringify(obj));
 }
 
+function isObject(obj) {
+    return typeof obj === 'object' && obj !== null;
+}
+
 /**
  * @param {HTMLElement} table
  * @param {{key: string}} params
- * @param {function({key: string}):void} changeCallback
+ * @param {false|function({key: (string|{type?: string, value: string})}):void} changeCallback
+ * @param {({buttonName:string}|null)} singleCallback
 */
-function createParamsTable(table, params, changeCallback) {
+function createParamsTable(table, params, changeCallback, singleCallback = null) {
     params = deepClone(params);
     /**@type {Array<string>}*/
     let paramsEntries = Object.entries(params);
 
     table.innerHTML = '';
+
+    let paramInputs = {};
 
     for (let [paramName, paramValue] of paramsEntries) {
         let optional = paramName.startsWith('?');
@@ -65,23 +100,37 @@ function createParamsTable(table, params, changeCallback) {
                 {
                     tag: 'td',
                     setup: function (element) {
-                        element.textContent = paramName + (optional ? '' : '*');
+                        element.textContent = paramName + ((optional || changeCallback === undefined) ? '' : '*');
                     }
                 },
                 {
                     tag: 'td',
-                    child: {
+                    childs: changeCallback === false ? [] : [{
                         tag: 'input',
                         setup: function (element) {
                             valueInput = element;
-                            element.setAttribute('type', 'text');
-                            element.setAttribute('value', paramValue);
+                            if (isObject(paramValue)) {
+                                element.setAttribute('type', 'type' in paramValue ? paramValue.type : 'text');
+                                element.setAttribute('value', paramValue.value);
+                            } else {
+                                element.setAttribute('type', 'text');
+                                element.setAttribute('value', paramValue);
+                            }
                             element.style.height = '100%';
                             element.style.width = '100%';
                         }
+                    }],
+                    setup: function (element) {
+                        if (changeCallback === false) {
+                            if (isObject(paramValue)) {
+                                element.textContent = paramValue.value;
+                            } else {
+                                element.textContent = paramValue;
+                            }
+                        }
                     }
                 },
-                {
+                ...(singleCallback !== null || changeCallback === false ? [] : [{
                     tag: 'td',
                     child: {
                         tag: 'input',
@@ -97,10 +146,52 @@ function createParamsTable(table, params, changeCallback) {
                                 let valueText = valueInput.value;
                                 valueText = valueText.trim();
                                 if (!optional && valueText.length === 0) {
-                                    alert('Ошибка изменения! Значение поля обязательно');
+                                    alert('РћС€РёР±РєР° СЃРѕС…СЂР°РЅРµРЅРёСЏ! Р—РЅР°С‡РµРЅРёРµ РїРѕР»СЏ РѕР±СЏР·Р°С‚РµР»СЊРЅРѕ');
                                     return;
                                 }
-                                params[(optional ? '?' : '') + paramName] = valueText;
+                                params[(optional ? '?' : '') + paramName] = valueText.length == 0 ? null : valueText;
+                                changeCallback(params);
+                            }
+                        }
+                    }
+                }])
+            ]
+        }));
+        paramInputs[paramName] = valueInput;
+    }
+    if (singleCallback !== null && changeCallback !== false) {
+        let paramsKeys = Object.keys(params);
+        table.appendChild(createTree({
+            tag: 'tr',
+            childs: [
+                {
+                    tag: 'td',
+                    setup: function (element) {
+                        element.setAttribute('colspan', '2');
+                    },
+                    child: {
+                        tag: 'input',
+                        setup: function (element) {
+                            element.setAttribute('type', 'button');
+                            element.setAttribute('value', singleCallback.buttonName);
+                            element.style.backgroundColor = '#00ff00';
+                            element.style.height = '30px';
+
+                            element.onclick = function () {
+                                for (let paramName of paramsKeys) {
+                                    let optional = paramName.startsWith('?');
+                                    if (optional) paramName = paramName.substring(1);
+                                    /**@type {HTMLElement}*/
+                                    let paramInput = paramInputs[paramName];
+                                    /**@type {string}*/
+                                    let paramText = paramInput.value;
+                                    paramText = paramText.trim();
+                                    if (!optional && paramText.length === 0) {
+                                        alert('РћС€РёР±РєР° СЃРѕС…СЂР°РЅРµРЅРёСЏ! Р—РЅР°С‡РµРЅРёРµ РїРѕР»СЏ "' + paramName +'" РѕР±СЏР·Р°С‚РµР»СЊРЅРѕ');
+                                        return;
+                                    }
+                                    params[(optional ? '?' : '') + paramName] = paramText.length === 0 ? null : paramText;
+                                }
                                 changeCallback(params);
                             }
                         }
@@ -113,9 +204,9 @@ function createParamsTable(table, params, changeCallback) {
 /**
  * @param {HTMLElement} table
  * @param {{key: string}} fields
- * @param {function({key: string}):void} changeCallback
+ * @param {false|function({key: string}):void} changeCallback
 */
-function createFieldsTable(table, fields, changeCallback) {
+function createFieldsTable(table, fields, changeCallback = false) {
     params = deepClone(params);
     /**@type {Array<string>}*/
     let fieldEntries = Object.entries(fields);
@@ -138,7 +229,7 @@ function createFieldsTable(table, fields, changeCallback) {
                         element.textContent = fieldValue;
                     }
                 },
-                {
+                ...(changeCallback === false ? [] : [{
                     tag: 'td',
                     child: {
                         tag: 'input',
@@ -154,10 +245,12 @@ function createFieldsTable(table, fields, changeCallback) {
                             }
                         }
                     }
-                }
+                }])
             ]
         }));
     }
+
+    if (changeCallback === false) return;
 
     /**@type {HTMLElement}*/
     var keyInput;
@@ -212,14 +305,14 @@ function createFieldsTable(table, fields, changeCallback) {
                             let valueText = valueInput.value;
                             valueText = valueText.trim();
                             if (keyText.length === 0 || valueText.length === 0) {
-                                alert('Ошибка добавления! Название или значение поля пустое');
+                                alert('РћС€РёР±РєР° СЃРѕС…СЂР°РЅРµРЅРёСЏ! РќР°Р·РІР°РЅРёРµ РёР»Рё Р·РЅР°С‡РµРЅРёРµ РїРѕР»СЏ РїСѓСЃС‚РѕРµ');
                                 return;
                             }
                             if (keyText in fields) {
-                                alert('Ошибка добавления! Название поля совпадает с существующим полем');
+                                alert('РћС€РёР±РєР° СЃРѕС…СЂР°РЅРµРЅРёСЏ! РќР°Р·РІР°РЅРёРµ РїРѕР»СЏ СЃРѕРІРїР°РґР°РµС‚ СЃ СЃСѓС‰РµСЃС‚РІСѓСЋС‰РёРј РїРѕР»РµРј');
                                 return;
                             }
-                            fields[keyText] = valueText;
+                            fields[keyText] = valueText.length === null ? null : valueText;
                             changeCallback(fields);
                         }
                     }
@@ -227,4 +320,47 @@ function createFieldsTable(table, fields, changeCallback) {
             }
         ]
     }));
+}
+/**
+ * @param {HTMLElement} table
+ * @param {Array<{id:string, show:Array<string>}>} hrefs
+ * @param {false|function(string):void} hrefCallback
+*/
+function createHrefTable(table, hrefs, hrefCallback = false) {
+    params = deepClone(params);
+    /**@type {Array<string>}*/
+
+    table.innerHTML = '';
+
+    for (let href of hrefs) {
+        let showArray = [];
+        let id = href.id;
+        for (let showItem of href.show) {
+            showArray.push({
+                tag: 'td',
+                setup: function (element) {
+                    element.textContent = showItem;
+                }
+            });
+        }
+        table.appendChild(createTree({
+            tag: 'tr',
+            childs: [
+                ...showArray,
+                ...(hrefCallback === false ? [] : [{
+                    tag: 'td',
+                    child: {
+                        tag: 'input',
+                        setup: function (element) {
+                            element.setAttribute('type', 'button');
+                            element.setAttribute('value', 'РџРµСЂРµР№С‚Рё');
+                            element.onclick = function () {
+                                hrefCallback(id);
+                            }
+                        }
+                    }
+                }])
+            ]
+        }));
+    }
 }
